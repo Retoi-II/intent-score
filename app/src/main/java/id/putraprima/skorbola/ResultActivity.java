3ff3be99e84459fc31e446a0901422fb79d9716c
Lannzu Sambool <azizzulia1@gmail.com>
2022-11-18 08:24:41 +0700

    Shift G - H1D020013 - Zulia Aziz Nugroho

--- app/src/main/java/id/putraprima/skorbola/ResultActivity.java
+++ app/src/main/java/id/putraprima/skorbola/ResultActivity.java
@@ -2,13 +2,24 @@ package id.putraprima.skorbola;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private TextView tvTextView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        getSupportActionBar().hide();
+
+        tvTextView3 = findViewById(R.id.textView3);
+        tvTextView3.setText(getIntent().getExtras().getString("winner"));
+
     }
 }
